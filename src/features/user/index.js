d6b6891cc43ed3bdd80c03b1b2020a5a01a0d441
Zesk <nhatkhang25092005@gmail.com>
2025-09-10 14:17:34 +0700

    file name

--- src/features/user/index.js
+++ src/features/user/index.js
@@ -1,4 +1,4 @@
-export {default as Login} from "./login/Login.jsx"
+export {default as Login} from "./login/Login"
 export {default as Register} from "./register/Register.jsx"
 export {default as Verify} from "./verify/Verify.jsx"
 export {default as ForgotPassword} from "./forgotPassword/ForgotPassword.jsx"
